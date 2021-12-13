b6d503a1115f9faee84172661a9ed5c93022ec5d
Marlon Salles <72519815+MarlonSallesSevero@users.noreply.github.com>
2021-12-12 23:08:02 -0300

    Add files via upload

---
+++ ContaBancaria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ContaBancaria
+{
+    public class ContaBancaria
+    {
+        public int Nconta { get; private set; } // Propriedades/Atributos da Classe
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+        public double Deposito(double valor) // Metodo
+        {
+            Saldo = valor + Saldo;
+            return Saldo;
+        }
+        public double Saque(double valor) // Metodo
+        {
+            Saldo = (Saldo - valor) - 5;
+            return Saldo;
+        }
+        public ContaBancaria(int nconta, string nome) // Contrutor
+        {
+            Nconta = nconta;
+            Nome = nome;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace ContaBancaria
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int numConta,opera = 0,contr;
+            string nome;
+            double vl;
+            ContaBancaria a;
+            Console.WriteLine("Informe o numero da conta:");
+            numConta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o nome do titular da conta:");
+            nome = Console.ReadLine();
+            a = new ContaBancaria(numConta, nome);
+
+            Console.WriteLine("*------------------------------*");
+
+            Console.WriteLine("Deseja realizar um deposito inicial?\n1-SIM\n2-NAO");
+            contr = int.Parse(Console.ReadLine());
+            if (contr == 1)
+            {
+                Console.WriteLine("Informe o valor a depositar");
+                vl = double.Parse(Console.ReadLine());
+                a.Deposito(vl);
+                Console.WriteLine($"{a.Nconta}:{a.Nome}:{a.Saldo}");
+            }
+            else if (contr == 2)
+            {
+                Console.WriteLine("ok!!");
+                Console.WriteLine($"{a.Nconta}:{a.Nome}:{a.Saldo}\n*------------------------------*");
+            }
+            else
+            {
+                Console.WriteLine("Operacao Invalida!!");
+                Console.WriteLine($"{a.Nconta}:{a.Nome}:{a.Saldo}\n*------------------------------*");
+            }
+
+            Console.WriteLine("*------------------------------*");
+            while (opera != 2) {
+                Console.WriteLine("Informe uma operacao:");
+                Console.WriteLine("1-DEPOSITO\n2-SAQUE");
+                opera = int.Parse(Console.ReadLine());
+                if(opera == 1)
+                {
+                    Console.WriteLine("Informe o valor a depositar:");
+                    vl = double.Parse(Console.ReadLine());
+                    a.Deposito(vl);
+                    Console.WriteLine($"{a.Nconta}:{a.Nome}:{a.Saldo}\n*------------------------------*");
+                }
+                else if (opera == 2)
+                {
+                    Console.WriteLine("Informe o valor a Sacar:");
+                    vl = double.Parse(Console.ReadLine());
+                    a.Saque(vl);
+                    Console.WriteLine($"{a.Nconta}:{a.Nome}:{a.Saldo}\n*------------------------------*");
+                }
+                else
+                {
+                    Console.WriteLine("Opcao Invalida!!\n*------------------------------*");
+                }
+                Console.WriteLine("Quer Continuar?\n1-SIM\n2-NAO");
+                opera = int.Parse(Console.ReadLine());
+                Console.WriteLine("*------------------------------*");
+            }
+        }
+    }
+}
